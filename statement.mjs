9bd9a9c3b4d3b2a97f7a71f51c3fca57402833e0
akak4456 <akak4456@naver.com>
2020-12-29 13:41:00 +0900

    2

--- statement.mjs
+++ statement.mjs
@@ -6,6 +6,19 @@ export default function statement(invoice, plays) {
 
     for(let perf of invoice.performances){
         const play = plays[perf.playID];
+        let thisAmount = amountFor(perf,play);
+        volumeCredits += Math.max(perf.audience-30,0);
+        if("comedy" === play.type) volumeCredits += Math.floor(perf.audience/5);
+
+        result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience}석)\n`;
+        totalAmount += thisAmount;
+    }
+
+    result += `총액: ${format(totalAmount/100)}\n`;
+    result += `적립 포인트: ${volumeCredits}점\n`;
+    return result;
+
+    function amountFor(perf,play){
         let thisAmount = 0;
         switch(play.type){
             case "tragedy":
@@ -24,14 +37,6 @@ export default function statement(invoice, plays) {
             default:
                 throw new Error(`알 수 없는 장르: ${play.type}`);
         }
-        volumeCredits += Math.max(perf.audience-30,0);
-        if("comedy" === play.type) volumeCredits += Math.floor(perf.audience/5);
-
-        result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience}석)\n`;
-        totalAmount += thisAmount;
+        return thisAmount;
     }
-
-    result += `총액: ${format(totalAmount/100)}\n`;
-    result += `적립 포인트: ${volumeCredits}점\n`;
-    return result;
 }
