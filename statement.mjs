18ba28e883c86b22986991faaf86220f7deb1acd
akak4456 <akak4456@naver.com>
2020-12-29 14:23:32 +0900

    23

--- statement.mjs
+++ statement.mjs
@@ -1,11 +1,12 @@
 export default function statement(invoice,plays){
     const statementData = {};
     statementData.customer = invoice.customer;
-    return renderPlanText(statementData,invoice,plays);
+    statementData.performances = invoice.performances;
+    return renderPlanText(statementData,plays);
 }
-function renderPlanText(data, invoice, plays) {
+function renderPlanText(data, plays) {
     let result = `청구 내역 (고객명: ${data.customer})\n`;
-    for(let perf of invoice.performances){
+    for(let perf of data.performances){
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     }
 
@@ -14,14 +15,14 @@ function renderPlanText(data, invoice, plays) {
     return result;
     function totalAmount(){
         let result = 0;
-        for(let perf of invoice.performances){
+        for(let perf of data.performances){
             result += amountFor(perf);
         }
         return result;
     }
     function totalVolumeCredits(){
         let result = 0;
-        for(let perf of invoice.performances){
+        for(let perf of data.performances){
             result += volumeCreditsFor(perf);
         }
         return result;
