3f4ec1dd215674bcf37c89c19d61ba297c5f1fd1
akak4456 <akak4456@naver.com>
2020-12-29 14:18:08 +0900

    18

--- statement.mjs
+++ statement.mjs
@@ -3,12 +3,11 @@ export default function statement(invoice, plays) {
     for(let perf of invoice.performances){
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     }
-    let totalAmount = appleSauce();
 
-    result += `총액: ${usd(totalAmount)}\n`;
+    result += `총액: ${usd(totalAmount())}\n`;
     result += `적립 포인트: ${totalVolumeCredits()}점\n`;
     return result;
-    function appleSauce(){
+    function totalAmount(){
         let totalAmount = 0;
         for(let perf of invoice.performances){
             totalAmount += amountFor(perf);
