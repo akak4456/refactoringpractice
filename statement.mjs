1aa6c428af326c6fa104d06682d35e68c86206d7
akak4456 <akak4456@naver.com>
2020-12-29 14:25:09 +0900

    24

--- statement.mjs
+++ statement.mjs
@@ -1,7 +1,11 @@
 export default function statement(invoice,plays){
     const statementData = {};
     statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances;
+    statementData.performances = invoice.performances.map(enrichPerformance);
+    function enrichPerformance(aPerformance){
+        const result = Object.assign({},aPerformance);
+        return result;
+    }
     return renderPlanText(statementData,plays);
 }
 function renderPlanText(data, plays) {
