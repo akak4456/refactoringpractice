feaf9f28c55d9bba0040be445b910c809e70ff62
akak4456 <akak4456@naver.com>
2020-12-30 14:49:55 +0900

    6

---
+++ mochaTest.mjs
@@ -0,0 +1,21 @@
+import Province from './Province.mjs';
+import assert from 'assert';
+function sampleProvinceData(){
+    return {
+        name:"Asia",
+        producers:[
+            {name:"Byzantium", cost:10, production:9},
+            {name:"Attalia",cost:12,production:10},
+            {name:"Sinope",cost:10,production:6},
+        ],
+        demand:30,
+        price:20
+    }
+}
+
+describe('province',function(){
+    it('shortfall',function(){
+        const asia = new Province(sampleProvinceData());
+        assert.equal(asia.shortfall,5);
+    })
+})
