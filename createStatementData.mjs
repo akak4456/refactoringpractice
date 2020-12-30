a76ef0dbfcd4588e77bccbf3621161cfb46c4c00
akak4456 <akak4456@naver.com>
2020-12-30 13:06:48 +0900

    4

--- createStatementData.mjs
+++ createStatementData.mjs
@@ -4,22 +4,7 @@ class PerformanceCalculator{
         this.play = aPlay;
     }
     get amount(){
-        let result = 0;
-        switch(this.play.type){
-            case "tragedy":
-                throw '오류 발생';
-                break;
-            case "comedy":
-                result = 30000;
-                if(this.performance.audience > 20){
-                    result += 10000 + 500 * (this.performance.audience-20);
-                }
-                result += 300 * this.performance.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르: ${this.play.type}`);
-        }
-        return result;
+        throw new Error('서브클래스에서 처리하도록 설계되었습니다.');
     }
     get volumeCredits(){
         let result = 0;
@@ -64,7 +49,14 @@ class TragedyCalculator extends PerformanceCalculator{
     }
 }
 class ComedyCalculator extends PerformanceCalculator{
-
+    get amount(){
+        let result = 30000;
+        if(this.performance.audience > 20){
+            result += 10000 + 500 * (this.performance.audience-20);
+        }
+        result += 300 * this.performance.audience;
+        return result;
+    }
 }
 
 function createPerformanceCalculator(aPerformance,aPlay){
